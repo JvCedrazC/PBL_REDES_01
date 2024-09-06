c6eed1c812fc17982ee484222752e19b2d21a30f
JvCedrazC <jvcarneiro@ecomp.uefs.br>
2024-09-06 14:12:54 -0300

    Update index_server.py

--- Teste/Teste_Threads/Server/index_server.py
+++ Teste/Teste_Threads/Server/index_server.py
@@ -2,13 +2,25 @@ import threading
 import socket
 clients = []
 
+def get_ipv4():
+    # Tenta criar uma conexão para obter o IP da máquina local
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # Conecta a um IP externo para obter o IP local (não será enviado nenhum dado)
+        s.connect(("8.8.8.8", 80))  # Usa o DNS do Google como referência
+        ipv4 = s.getsockname()[0]
+    except Exception as e:
+        ipv4 = "Não foi possível obter o IPv4"
+    finally:
+        s.close()
+    return ipv4
 
 def main():
 
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+    ipv4 = str(get_ipv4())
     try:
-        server.bind(('172.16.103.221', 777))
+        server.bind((ipv4, 777))
         server.listen(15)
     except:
         return print("Não foi possível iniciar o servidor")
